228c640ef88b807c36976a3c99f43cef58ed39f8
deepdhojak <deepdhojak@gmail.com>
2024-02-02 15:08:41 +0530

    some changes

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
- 
+
 const List = () => {
   const API_URL = 'https://api.tvmaze.com/search/shows?q=all';
 
@@ -20,39 +20,44 @@ const List = () => {
     };
     fetchData();
   }, []);
-  let navigate  = useNavigate();
-  // eslint-disable-next-line
-  const handleSubmit =  async (e)=>{
-    e.preventDefault();
-    navigate("/Summary");
+  let navigate = useNavigate();
 
-  }
 
   return (
     <div className="container my-3" >
-    <h1 className="text-center" >TV Shows</h1>
-    <div className="row" >
+      <h1 className="text-center" >TV Shows</h1>
+      <div className="row" >
 
-      {shows.map((show) => (
-        <div className="col-md-4" key={show.show.id}>
-          <h2 >{show.show.name}</h2>
-          {show.show.image && <img className="img-fluid rounded" src={show.show.image.medium} alt={show.show.name} />}
-          <div className="my-2 mb-0">
+        {shows.map((show) => (
+          <div className="col-md-4" key={show.show.id}>
+            <h2 >{show.show.name}</h2>
+            {show.show.image && <img className="img-fluid rounded" src={show.show.image.medium} alt={show.show.name} />}
+            <div className="my-2 mb-0">
               <p>
-                Language: {show.show.language}  
+                Language: {show.show.language}
               </p>
               <p>
-              Genres: {show.show.genres.join(', ')}
+                Genres: {show.show.genres.join(', ')}
               </p>
+            </div>
+            <p>Premiere Date: {show.show.premiered}</p>
+
+            <button type="button" onClick={() => {
+              localStorage.setItem('{show.show.summary}', show.show.summary);
+              localStorage.setItem('{show.show.name}', show.show.name);
+              localStorage.setItem('{show.show.language}', show.show.language);
+
+              localStorage.setItem('{show.show.premiered}', show.show.premiered);
+
+              navigate("/Summary");
+
+            }} className="btn btn-success">Summary</button>
+
+            <hr />
           </div>
-          <p>Premiere Date: {show.show.premiered}</p>
-          <button type="button" onClick={handleSubmit}  className="btn btn-success">Summary</button>
-
-          <hr />
-        </div>
-      ))}
-     </div>
-  </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,49 +1,48 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
 
-      const Summary = ({  }) => {
-  const [shows, setShow] = useState([]);
-
-  useEffect(() => {
-    const fetchShow = async () => {
-      try {
-        const response = await fetch(`https://api.tvmaze.com/shows/${shows.show.id}`);
-        const data = await response.json();
-        setShow(data);
-      } catch (error) {
-        console.error('Error fetching show details:', error);
-      }
-    };
-
-    fetchShow();
-  }, []);
-
-  if (!shows) {
-    return <div>Loading...</div>;
-  }
+const Summary = () => {
+  let fetchShowSummary = localStorage.getItem('{show.show.summary}') || '';
+  let date = localStorage.getItem('{show.show.premiered}') || '';
+  let Language = localStorage.getItem('{show.show.language}') || '';
+  let Name = localStorage.getItem('{show.show.name}') || '';
 
   return (
     <div>
-      <h2>Show Details</h2>
-      <h3>{ name}</h3>
-      <p>{summary}</p>
-      <div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+       <h1 className="text-center my-3">Summary</h1>
+      <p className="text-center my-3">{fetchShowSummary}</p>
+       <hr />
+      <div className="d-grid gap-2 col-6 mx-auto">
+
+        <button type="button" className="btn btn-primary my-5" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          Book Show
+        </button>
+
       </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
+
+      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">{Name}</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <p>
+                Language: {Language}
+              </p>
+              <p>
+                Premiere Date: {date}
+              </p>
+
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Book</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>           
     </div>
   );
 };
